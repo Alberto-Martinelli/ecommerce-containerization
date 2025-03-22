fa77520c1d9ff90fba2f389f191fe10fc6d5bfdf
Alberto Martinelli s329095 <s329095@studenti.polito.it>
2025-03-22 12:12:04 +0100

    Completed the split of backend2

--- backend2/backend2/settings.py
+++ backend2/backend2/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for backend2 project.
 
@@ -117,7 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/' # to load media files on admin page as well (changes in url.py file also, project level)
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # new
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
